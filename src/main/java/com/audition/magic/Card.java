501936bba7c05186778a82263d4959d6cbad23eb
Matt Gleason <gleason.matt@protonmail.com>
2020-04-12 13:19:54 -0400

    TradingGame: constructors

--- src/main/java/com/audition/magic/Card.java
+++ src/main/java/com/audition/magic/Card.java
@@ -1,4 +1,9 @@
 package com.audition.magic;
 
 public class Card {
+    private int manaCost;
+
+    Card(int manaCost) {
+        this.manaCost = manaCost;
+    }
 }
--- src/main/java/com/audition/magic/Player.java
+++ src/main/java/com/audition/magic/Player.java
@@ -1,7 +1,26 @@
 package com.audition.magic;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+    * Each player starts the game with 30 Health and 0 Mana slots
+    * Each player starts with a deck of 20 Damage cards with the following Mana costs: 0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8
+    * From the deck each player receives 3 random cards has his initial hand
+*/
 public class Player {
+    private String name = "";
     private int health = 30;
     private int currentMana = 0;
     private int totalMana = 0;
+
+    private List<Card> deck = Stream.of(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8)
+            .map(Card::new)
+            .collect(Collectors.toList());
+
+    public void Player(String name) {
+        this.name = name;
+    }
+
 }
